b92b6b91ebf844e1a9af58ec816a86015c5cd05e
Eddie Rayner <eddie.rayner1@gmail.com>
2023-08-17 21:57:02 +0100

    Placeholder music added

---
+++ 1BitJam/Assets/Scripts/GameController.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+    SceneManager sceneManager;
+    public AudioSource topMenuMusic;
+    public AudioSource mainMusic;
+
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("OnSceneLoaded: " + scene.name);
+        Debug.Log(mode);
+
+        //Need to add fade out audio ? or we can just use 1 track for the whole game and ignore this script - unsure 
+
+        if(scene.name=="Top Menu" && !topMenuMusic.isPlaying)
+        {
+            topMenuMusic.Play();
+            mainMusic.Stop();
+        }
+        else if(scene.name.Substring(0,5)=="Level" && !mainMusic.isPlaying)
+        {
+            mainMusic.Play();
+            topMenuMusic.Stop();           
+        }
+    }
+
+    
+}
