c9c8fe4c303fd3a0d73d5f7eeb74c34f7e2098a6
Eddie Rayner <eddie.rayner1@gmail.com>
2023-08-17 12:11:58 +0100

    World Dictionary added for shaders

--- 1BitJam/Assets/Scripts/CameraShader.cs
+++ 1BitJam/Assets/Scripts/CameraShader.cs
@@ -1,21 +1,42 @@
 using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 [ExecuteInEditMode]
 public class TwoColourEffect : MonoBehaviour {
     public float intensity;
-    public float foregroundR = 1;
-    public float foregroundG = 1;
-    public float foregroundB = 1;
-    public float backgroundR = 0;
-    public float backgroundG = 0;
-    public float backgroundB = 0;
+    float foregroundR = 1;
+    float foregroundG = 1;
+    float foregroundB = 1;
+    float backgroundR = 0;
+    float backgroundG = 0;
+    float backgroundB = 0;
     private Material material;
+
+    Dictionary<int, int[]> worldToColour = new Dictionary<int, int[]>()
+    {
+        // world colours - intensity, foregroundR, foregroundG, foregroundB, backgroundR, backgroundR, backgroundB
+        {1, new int[] {1,1,1,1,0,0,0} },
+        {2, new int[] {1,1,0,0,0,0,0} } // TEMP just to test if colours were working (they do!)
+    };
+    
+    public int world = 1;
     // Creates a private material used to the effect
     void Awake ()
     {
         material = new Material( Shader.Find("Hidden/BWDiffuse") );
     }
-    
+
+    private void Start()
+    {
+        intensity = worldToColour[world][0];
+        foregroundR = worldToColour[world][1];
+        foregroundG = worldToColour[world][2];
+        foregroundB = worldToColour[world][3];
+        backgroundR = worldToColour[world][4];
+        backgroundG = worldToColour[world][5];
+        backgroundB = worldToColour[world][6];
+    }
+
     // Postprocess the image
     void OnRenderImage (RenderTexture source, RenderTexture destination)
     {
