4f5b65a0fb959bf5cafb799e0064c28ca746dcbb
djangopinter <djangopinter@gmail.com>
2024-02-14 14:23:32 +0000

    new background decorations

--- 1BitJam/Assets/Scripts/FloorDecorSprite.cs
+++ 1BitJam/Assets/Scripts/FloorDecorSprite.cs
@@ -4,21 +4,8 @@ using UnityEngine;
 
 public class FloorDecorSprite : MonoBehaviour
 {
-    /*Dictionary<int, float[]> worldSize = new Dictionary<int, float[]>()
-    {
-        // world sizes
-        {0, new float[] {7,5} }, // W1/2 Secret Levels
-        {1, new float[] {7,5} }, // W1
-        {2, new float[] {7,7} }, // W3 - Glass
-        {3, new float[] {12,7} }, // W4 - Moving Light
-        {4, new float[] {16,9} }, // W6 - Detector
-        {5, new float[] {7,5} }, // W2 - Long Blocks
-        {6, new float[] {12,7} }, // W5 - Spikes
-        {7, new float[] {16,9} }, // W7 - unmade :)
-        {8, new float[] {7,7} }, // W3 Secret Levels
-        {9, new float[] {12,7} }, // W4/5 Secret Levels
-        {10, new float[] {16,9} } // W6/7 Secret Levels
-    };*/
+    public Sprite[] FloorSprites;
+    int[] worldSel = { 0,0,1,2,3,0,2,3,1,2,3 };
 
     int world = 1;
     // Start is called before the first frame update
@@ -26,6 +13,7 @@ public class FloorDecorSprite : MonoBehaviour
     {
         GameObject cam = GameObject.Find("Main Camera");
         world = cam.gameObject.GetComponent<TwoColourEffect>().world;
+        GetComponent<SpriteRenderer>().sprite = FloorSprites[worldSel[world]];
         //Debug.Log(world);
         //transform.localScale = new Vector3(worldSize[world][0],worldSize[world][1],1);
     }
