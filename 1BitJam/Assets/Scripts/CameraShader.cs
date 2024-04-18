2213b08c05f4b8a1048cf1ff65e93c79b319e336
djangopinter <djangopinter@gmail.com>
2024-04-18 12:11:20 +0100

    overworld ++

--- 1BitJam/Assets/Scripts/CameraShader.cs
+++ 1BitJam/Assets/Scripts/CameraShader.cs
@@ -20,7 +20,7 @@ public class TwoColourEffect : MonoBehaviour {
         // world colours - intensity, foreground RGB, midground RGB, background RGB
         {0, new float[] {1,170,35,45,90,90,90,40,40,40} }, // W1/2 Secret Levels
         {1, new float[] {1,170,170,170,70,70,70,40,40,40} }, // W1
-        {2, new float[] {1,200,220,175,60,90,130,10,50,90} }, // W3 - Glass
+        {2, new float[] {1,190,220,210,70,110,140,10,50,90} }, // W3 - Glass
         {3, new float[] {1,225,180,100,75,110,80,35,90,25} }, // W4 - Moving Light
         {4, new float[] {1,230,140,170,120,120,160,70,50,120} }, // W6 - Detector
         {5, new float[] {1,190,180,170,70,80,90,50,30,30} }, // W2 - Long Blocks
