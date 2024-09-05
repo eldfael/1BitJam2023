8adaed95c12f6578e9b6cbad6345f573e3a64fee
djangopinter <djangopinter@gmail.com>
2024-09-05 16:03:03 +0100

    w7 world bg & temp splash

--- 1BitJam/Assets/Scenes/Menus/SplashScript.cs
+++ 1BitJam/Assets/Scenes/Menus/SplashScript.cs
@@ -1,14 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class SplashScript : MonoBehaviour
 {
+    public Animator fadeIn;
 
     void Start()
     {
         Debug.Log("Splash Loaded");
-        SceneManager.LoadScene("Top Menu");   
+        StartCoroutine(TopMenu());
+    }
+    IEnumerator TopMenu()
+    {
+        yield return new WaitForSeconds(1);
+        fadeIn.SetTrigger("NextLevel");
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene("Top Menu"); 
     }
 }
